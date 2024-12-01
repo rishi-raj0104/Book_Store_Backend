92fb0cb14226b86c03f647554db4c5af0e191c39
Rishi Raj <rishi.rishiraj12@gmail.com>
2024-12-01 21:27:27 +0530

    second commit

--- index.jsx
+++ index.jsx
@@ -7,7 +7,7 @@ require('dotenv').config();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://book-store-mu-two.vercel.app/'],
     credentials: true
 }))
 // routes
