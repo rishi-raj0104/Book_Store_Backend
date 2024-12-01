cb703d378b2325ec4e0dd66fa113cb76cc9ab76a
Rishi Raj <rishi.rishiraj12@gmail.com>
2024-12-01 21:31:30 +0530

    first commit

--- index.jsx
+++ index.jsx
@@ -1,41 +1,45 @@
 const express = require('express');
-const app = express();
 const mongoose = require("mongoose");
-const cors = require('cors'); 
+const cors = require('cors');
 require('dotenv').config();
-// middleware
-app.use(express.json());
 
+// Initialize express app
+const app = express();
+
+// Middleware
+app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173','https://book-store-mu-two.vercel.app/'],
-    credentials: true
-}))
-// routes
+  origin: ['http://localhost:5173', 'https://book-store-mu-two.vercel.app/'],
+  credentials: true
+}));
+
+// Routes
 const bookRoutes = require('./src/books/book.route');
 const orderRoutes = require("./src/orders/order.route");
-const userRoutes =  require("./src/users/user.route");
+const userRoutes = require("./src/users/user.route");
 const adminRoutes = require("./src/stats/admin.stats");
 const paymentRoutes = require("./src/payment/paymentroute");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 
-const port = process.env.PORT || 5000;
 app.use('/', (req, res) => {
-  res.send('Welcome to world of routes');
-})
+  res.send('Welcome to the world of routes');
+});
+
+// MongoDB connection
 async function main() {
+  try {
     await mongoose.connect(process.env.DB_URL);
-    app.use("/", (req, res) => {
-      res.send("Book Store Server is running!");
-    });
+    console.log("Mongodb connected successfully!");
+  } catch (err) {
+    console.log(err);
   }
+}
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+module.exports = app;
